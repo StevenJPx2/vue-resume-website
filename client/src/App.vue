b7a7987fa41e5ac1a4bd64aaf3af4d18ebb5c382
Steven John <stevenjohn@Stevens-MacBook-Pro.local>
2020-01-13 19:19:52 +0530

    Updated

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,79 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Steven John</h1>
+    <div class="container flex">
+      <Icon _class="fab fa-medium" desc="Medium" />
+      <Icon _class="fab fa-codepen" desc="Codepen" />
+      <Icon _class="fab fa-github" desc="Github" />
+      <Icon _class="fas fa-file" desc="Resume" />
+    </div>
+    <div class="container grid">
+      <div class="heading">
+        <h1>Projects</h1>
+        <hr />
+      </div>
+      <div class="card-grid">
+        <Card :key="i" v-for="i in 10" :class="i == 1 ? 'head-card' : ''" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Icon from "./components/Icon.vue";
+import Card from "./components/Card.vue";
 
 export default {
-  name: "app",
+  name: "App",
   components: {
-    HelloWorld
+    Icon,
+    Card
   }
 };
 </script>
 
 <style>
+h1 {
+  text-align: center;
+}
+
+hr {
+  color: #fff;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+}
+
+.container {
+  text-align: center;
+  margin: 60px auto 0px auto;
+  width: 1200px;
+}
+
+.flex {
+  display: flex;
+}
+
+.grid {
+  display: grid;
+}
+
+.heading {
+  grid-column: 1/ 1;
+}
+
+.card-grid {
+  grid-row
+  display: grid;
+  grid-template-columns: repeat(2, auto);
+  grid-gap: 15px;
+}
+
+.head-card {
+  grid-column: 1 / 2;
 }
 </style>
---
+++ client/src/components/Card.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="card">
+    <img src="https://source.unsplash.com/500x500" alt="#" />
+    <div class="card-body">
+      <h2>Stuff</h2>
+      <p>This is the thing.</p>
+      <a href="#" class="button">Click here for more</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card"
+};
+</script>
+
+<style scoped>
+.card {
+  border: 1px #eeeeee solid;
+  border-radius: 5px;
+  display: grid;
+  grid-template-columns: 2fr 3fr;
+  grid-column-gap: 15px;
+}
+
+img {
+  border-radius: 5px 0px 0px 5px;
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+}
+
+h2 {
+  margin-bottom: 3px;
+}
+
+a {
+  text-decoration: none;
+  display: table;
+}
+
+a:visited {
+  color: black;
+}
+
+.button {
+  font-size: 14px;
+  font-family: 'Hack', 'Source Code Pro', monospace;
+  padding: 13px;
+  border: 1px black solid;
+  border-radius: 5px;
+  text-transform: uppercase;
+  color: black;
+  transition: all ease-out 0.2s;
+}
+
+.button:hover {
+  background-color: black;
+  color: white;
+}
+
+
+.card-body {
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+}
+</style>
