5e29bc1128bcc6713d3824c0a864e4f4e0e529db
Steven John <stevenjohn@Stevens-MacBook-Pro.local>
2020-01-14 19:19:56 +0530

    Sttarted building server-side

---
+++ server/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+
+# configuration
+DEBUG = True
+
+# instantiate the app
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+
+@app.route('/projects', methods=['GET'])
+def return_projects():
+    return jsonify('projects')
+
+
+if __name__ == '__main__':
+    app.run()
