2ae0dedb30efac6b8a934eb04fcf53de6f67ba97
Steven John <stevenjohn@Stevens-MacBook-Pro.local>
2020-01-17 20:14:41 +0530

    Fixed loose code

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
-from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 CORS(app, resources={r'/*': {'origins': '*'}})
-db = MongoEngine(app)
 
 from app import models, server
