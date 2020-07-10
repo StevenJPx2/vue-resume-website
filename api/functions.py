923bae953c0fbf63a5eeb31a7681498ce9e6c691
Steven John <stevenjpx@gmail.com>
2020-07-10 18:07:17 +0530

    Deleted all api files for a fresh Actix Rust reboot

--- api/functions.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-
-def access_secrets(file):
-    return Path(f".secrets/{file}").open().read().strip()
--- api/startup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-gunicorn --worker-class eventlet -w 1 --bind localhost:9000 app:app
