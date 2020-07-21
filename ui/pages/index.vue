6e4b10903020dbf6521e1216ec8b1370386e1009
Steven John <stevenjpx@gmail.com>
2020-07-21 17:27:17 +0530

    Adding all the new files

---
+++ ui/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="body">
+    <h1 class="font-sans text-6xl font-black">Steven John</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style></style>
--- ui/src/views/404.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div id="app"><h1>404 Not Found</h1></div>
-</template>
-
-<script>
-export default {
-  name: "404"
-};
-</script>
