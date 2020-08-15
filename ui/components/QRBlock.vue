77a0c0723e7132033bf2bad36f5b6ace62156150
Steven John <stevenjpx@gmail.com>
2020-08-15 11:00:17 +0530

    Created all the new blocks

---
+++ ui/components/QRBlock.vue
@@ -0,0 +1,10 @@
+<template>
+  <h3 contenteditable>Edit here...</h3>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
+
+<style lang="scss" scoped></style>
