c7b1d2694ff5fc13df616ded472e3bfd1f04631c
Steven John <stevenjpx@gmail.com>
2020-12-03 10:14:01 +0530

    (feat): Added dynamic Github repo tracker, and fixed minor bugs

---
+++ components/Icon.vue
@@ -0,0 +1,16 @@
+<template>
+  <fa :icon="[type, name]"></fa>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    name: String,
+    type: {
+      type: String,
+      default: 'fas',
+    },
+  },
+})
+</script>
