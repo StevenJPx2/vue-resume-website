c248f15a63adcb590bc9efa6219f680934124bc7
Steven John <stevenjpx2@gmail.com>
2023-01-17 19:49:19 +0530

    fix(home-footer): nuxt-link

--- components/home/Footer.vue
+++ components/home/Footer.vue
@@ -9,42 +9,25 @@ const props = defineProps({
 </script>
 <template>
   <div class="h-screen relative">
-    <directus-img
-      id="yellowEllipse"
-      :sizes="[1536]"
-      :img-attrs="{
-        class:
-          'pointer-events-none yellow-blob absolute scale-[1.5] top-[-13%] md:scale-100 md:left-[-28%] md:top-[-27%] z-[-1]',
-      }"
-    />
-    <directus-img
-      id="yellowEllipse"
-      :sizes="[1536]"
-      :img-attrs="{
-        class:
-          'pointer-events-none yellow-blob absolute scale-[1.5] bottom-[-13%] md:scale-100 md:right-[-28%] md:bottom-[-27%] z-[-1]',
-      }"
-    />
+    <directus-img id="yellowEllipse" :sizes="[1536]" :img-attrs="{
+      class:
+        'pointer-events-none yellow-blob absolute scale-[1.5] top-[-13%] md:scale-100 md:left-[-28%] md:top-[-27%] z-[-1]',
+    }" />
+    <directus-img id="yellowEllipse" :sizes="[1536]" :img-attrs="{
+      class:
+        'pointer-events-none yellow-blob absolute scale-[1.5] bottom-[-13%] md:scale-100 md:right-[-28%] md:bottom-[-27%] z-[-1]',
+    }" />
     <div
       class="mt-[15vw] h-full grid grid-rows-[max-content,max-content] place-content-center gap-y-[5vw] md:gap-y-[3vw]"
-      :class="[commonPadding]"
-    >
-      <h2
-        class="h1 text-[25vw] md:text-[20vw] text-center leading-[0.8] place-self-center"
-      >
+      :class="[commonPadding]">
+      <h2 class="h1 text-[25vw] md:text-[20vw] text-center leading-[0.8] place-self-center">
         {{ props.tagline }}
       </h2>
-      <div
-        class="flex flex-wrap justify-self-center gap-[7vw] md:gap-[4vw] px-[12vw] md:px-0"
-      >
-        <button
-          v-for="{ icon, id, link } in props.links"
-          :key="id"
-          :to="link"
-          class="opacity-90 hover:opacity-100 text-[9vw] md:text-[4vw] transition-transform duration-300 ease-out-quart hover:scale-125"
-        >
+      <div class="flex flex-wrap justify-self-center gap-[7vw] md:gap-[4vw] px-[12vw] md:px-0">
+        <nuxt-link v-for="{ icon, id, link } in props.links" :key="id" :to="link"
+          class="opacity-90 hover:opacity-100 text-[9vw] md:text-[4vw] transition-transform duration-300 ease-out-quart hover:scale-125">
           <icon :name="icon" />
-        </button>
+        </nuxt-link>
       </div>
     </div>
   </div>
